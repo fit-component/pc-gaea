741fb4201284616c79516b287259dc093d42d318
huangziyi01 <huangziyi01@baidu.com>
2016-06-07 13:02:39 +0800

    quick push

--- src/gaea/sidebar/dom-tree/index.tsx
+++ src/gaea/sidebar/dom-tree/index.tsx
@@ -1,9 +1,19 @@
 import * as React from 'react'
 import TreeElement from './tree-element'
+import connect from '../../utils/connect'
 import './index.scss'
 
+@connect(
+    (state: any) => {
+        return {
+            rootPropsStore: state.rootProps.toJS()
+        }
+    },
+    {}
+)
 export default class Sidebar extends React.Component <any ,any> {
     render() {
+        console.log(this.props['rootProps'])
         return (
             <div className="_namespace">
                 <TreeElement/>
