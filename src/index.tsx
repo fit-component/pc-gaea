3966af201979b44313d3f3a5265525ddc21f8695
huangziyi01 <huangziyi01@baidu.com>
2016-08-23 23:04:11 +0800

    quick push

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 /// <reference path="../../../../typings/tsd.d.ts" />
 import 'font-awesome/css/font-awesome.css'
 import Gaea from './gaea/gaea.component'
-//import Preview from './preview'
+import Preview from './preview/preview.component'
 
 export default Gaea
-//export {Gaea, Preview}
+export {Gaea, Preview}
